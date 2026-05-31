2bad97e1d83058ae00ee48f3f5960c019f53fcf1
BuzGibi1i <buzgibi1i@proton.me>
2026-05-31 19:17:58 +0200

    Delete src/main/kotlin/com directory

--- src/main/kotlin/com/example/SineKabukProvider.kt
+++
@@ -1,6 +0,0 @@
-// Dosyanın en üstüne bu annotation ŞART:
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-
-@CloudstreamPlugin
-class SineKabukProvider : MainAPI() {
-   // ... geri kalan kod aynı
