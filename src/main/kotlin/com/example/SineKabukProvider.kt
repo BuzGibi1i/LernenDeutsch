9ff27789a34e88dbe9dec8d588b46fd2c2a31389
BuzGibi1i <buzgibi1i@proton.me>
2026-05-31 19:17:09 +0200

    Create SineKabukProvider.kt

---
+++ src/main/kotlin/com/example/SineKabukProvider.kt
@@ -0,0 +1,6 @@
+// Dosyanın en üstüne bu annotation ŞART:
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class SineKabukProvider : MainAPI() {
+   // ... geri kalan kod aynı
