c7c974c373dd035f70d88e3f8c096f0f6b9f79c3
BuzGibi1i <buzgibi1i@proton.me>
2026-05-31 19:27:43 +0200

    Create build.gradle.kts

---
+++ LernenDeutschProvider/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    language    = "tr"
+    name        = "LernenDeutsch"
+    description = "Almanca öğrenme içerikleri"
+    authors     = listOf("BuzGibİ1i")
+    status      = 1
+    tvTypes     = listOf("Movie")
+    iconUrl     = "https://raw.githubusercontent.com/BuzGibi1i/LernenDeutsch/builds/LernenDeutschProvider/icon.png"
+}
