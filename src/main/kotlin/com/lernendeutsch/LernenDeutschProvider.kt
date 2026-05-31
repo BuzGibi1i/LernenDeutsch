d1d293222128384c50c8a7ae22623ac119142023
BuzGibi1i <buzgibi1i@proton.me>
2026-05-31 19:20:25 +0200

    Create LernenDeutschProvider.kt

---
+++ src/main/kotlin/com/lernendeutsch/LernenDeutschProvider.kt
@@ -0,0 +1,6 @@
+// Dosyanın en üstüne bu annotation ŞART:
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class LernenDeutschProvider : MainAPI() {
+   // ... geri kalan kod aynı
